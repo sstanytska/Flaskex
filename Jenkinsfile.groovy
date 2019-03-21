cafb76055da80c6b6dd2a54941dcc65b5bf559c1
sstanytska <stanytskasofia@gmail.com>
2019-03-20 19:58:28 -0500

    added flaskex service

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,14 +7,22 @@ properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please g
     stage("Clone a repo"){
         git 'https://github.com/sstanytska/Flaskex.git'
     }
-    stage("Copy files"){
-        sh "scp  -r * ec2-user@${IP}:/tmp/"
-    }
     stage("Install requirements"){
         sh "ssh ec2-user@${IP}   sudo pip install -r /tmp/requirements.txt"
     }
     stage("Run App"){
         sh "ssh ec2-user@${IP}    python /tmp/app.py"
     }
-
+    stage("Copy files"){
+        sh "scp  -r * ec2-user@${IP}:/tmp/"
+    }
+    stage("Move files to /flesk"){
+        sh "ssh ec2-user@${IP}      sudo pip install -r /klask/requirements.txt"
+    }
+    stage("Move service to /etc"){
+        sh "ssh ec2-user@${IP}      sudo /flaskex/flaskex.service /etc/system/system"
+    }
+    stage("Start service"){
+        sh "ssh ssh ec2-user@${IP} sudo systemctl start flaskex"
+    }
 }
